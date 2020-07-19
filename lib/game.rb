de6f3236161205845a393717f9a9167bedd57cf3
chook <dylanw212@gmail.com>
2020-07-19 13:34:50 +1000

    add working files

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,45 @@ class Game
 
   def initialize
     @p1 = Player.new('Player 1', 'X')
-    @p1 = Player.new('Player 2', 'O')
-    @board = Board.new
+    @p2 = Player.new('Player 2', 'O')
+    @board = nil
   end
 
+  # Welcome message / title
+  def welcome
+    puts 'Welcome to connect 4!'
+    # menu can be inserted here later
+    new_game
+  end
+
+  def menu
+    # TODO
+  end
+
+  def new_game
+    self.board = Board.new
+    board.draw_grid
+    current_player = p1
+    # until game_over? do
+      # play_round(current_player)
+      # current_player = swap_player
+    # end
+    # ending
+  end
+
+  def play_round(player)
+    # get input
+    # check / get input until it's right
+    # update board data
+    # redraw board
+
+  end
+
+  def game_over?
+    #board.won? || board.drawn?
+  end
+
+  def swap_player(player)
+    player == p1 ? p2 : p1
+  end
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,4 @@ require './lib/board.rb'
 require './lib/game.rb'
 require './lib/player.rb'
 
-board = Board.new
-
-board.draw_grid_proto
-
-#pp board.grid_helper
+Game.new.welcome
