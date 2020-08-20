4edcf697ed3abec9cc5e337b65114937b97f9b60
chook <dylanw212@gmail.com>
2020-08-20 15:11:58 +1000

    small style change

--- achive/player_temp.rb
+++ achive/player_temp.rb
@@ -1,4 +1,4 @@
 Player = Struct.new(:name, :symbol)
 
-player1 = Player.new('Player 1', 'X')
-player2 = Player.new('Player 2', 'O')
+_player1 = Player.new('Player 1', 'X')
+_player2 = Player.new('Player 2', 'O')
