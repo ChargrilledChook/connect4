458e345ee5b0aa06099a3d5efeda57fdb3a85e39
chook <dylanw212@gmail.com>
2020-08-21 11:13:15 +1000

    add ascii title

--- lib/display.rb
+++ lib/display.rb
@@ -1,5 +1,17 @@
 # Strings go here
 module Display
+  def title
+    <<~HERE
+       _____                             _       ___
+      /  __ \                           | |     /   |
+      | /  \/ ___  _ __  _ __   ___  ___| |_   / /| |
+      | |    / _ \| '_ \| '_ \ / _ \/ __| __| / /_| |
+      | \__/\ (_) | | | | | | |  __/ (__| |_  \___  |
+       \____/\___/|_| |_|_| |_|\___|\___|\__|     |_/
+
+    HERE
+  end
+
   def welcome_msg
     'Welcome to Connect 4!'
   end
