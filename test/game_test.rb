79b24074d489991305cfa2bbab8ef198a8a8a458
chook <dylanw212@gmail.com>
2020-08-31 16:46:10 +1000

    add pseudocode for game tests

--- test/game_test.rb
+++ test/game_test.rb
@@ -2,4 +2,14 @@ require 'minitest/autorun'
 require_relative '../lib/game'
 
 class GameTest < Minitest::Test
+  # Loops if game not over
+
+  # Stops / calls game_over if game is over
+
+  # Swaps player correctly if game not over
+
+  # Select column gets sent
+
+  # Calls the correct ending message (player who won or tie)
+
 end
