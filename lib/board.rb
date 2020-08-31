3d506a81c0ad6f6fe2ec64dccdf123f6775e6c2f
chook <dylanw212@gmail.com>
2020-08-31 12:13:24 +1000

    connect new instance variables

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 # Holds data about gamestate
 class Board
+  attr_reader :empty_cell, :width, :height, :win
+
   attr_accessor :grid
 
   # These are mapped to arrays in order to calculate winning combos on diagonals.
@@ -12,14 +14,6 @@ class Board
                 [[1, 0], [2, 1], [3, 2], [4, 3], [5, 4]],
                 [[2, 0], [3, 1], [4, 2], [5, 3]]].freeze
 
-  # Default values for the playing grid
-  EMPTY_CELL = 0
-  WIDTH = 7
-  HEIGHT = 6
-
-  # Amount of tokens in a row required to win
-  WIN = 4
-
   def initialize
     @win = 4
     @height = 6
@@ -29,12 +23,12 @@ class Board
   end
 
   def build_grid
-    Array.new(HEIGHT) { Array.new(WIDTH, EMPTY_CELL) }
+    Array.new(height) { Array.new(width, empty_cell) }
   end
 
   def draw_grid
     render = grid.map { |line| line.map { |cell| " #{cell} " }.join }
-    render << (1..WIDTH).map { |cell| " #{cell} " }.join
+    render << (1..width).map { |cell| " #{cell} " }.join
     render
   end
 
@@ -43,14 +37,14 @@ class Board
   end
 
   def game_tied?
-    @grid.all? { |line| line.none?(EMPTY_CELL) }
+    @grid.all? { |line| line.none?(empty_cell) }
   end
 
   # Updates the grid with a drop down move. Will return true if succesful and false otherwise
   def input_move(column, player)
     placed = false
     grid.reverse_each do |x|
-      if x[column - 1] == EMPTY_CELL && placed == false
+      if x[column - 1] == empty_cell && placed == false
         x[column - 1] = player
         placed = true
       end
@@ -61,18 +55,18 @@ class Board
   private
 
   def horizontal_win?(player)
-    @grid.any? { |line| line.join.include?(player * WIN) }
+    @grid.any? { |line| line.join.include?(player * win) }
   end
 
   def vertical_win?(player)
-    @grid.transpose.any? { |line| line.join.include?(player * WIN) }
+    @grid.transpose.any? { |line| line.join.include?(player * win) }
   end
 
   def diag_win_right?(player)
-    DIAG_LINES.map { |line| line.map { |x, y| grid[x][y] } }.any? { |line| line.join.include?(player * WIN) }
+    DIAG_LINES.map { |line| line.map { |x, y| grid[x][y] } }.any? { |line| line.join.include?(player * win) }
   end
 
   def diag_win_left?(player)
-    DIAG_LINES.map { |line| line.map { |x, y| grid.reverse[x][y] } }.any? { |line| line.join.include?(player * WIN) }
+    DIAG_LINES.map { |line| line.map { |x, y| grid.reverse[x][y] } }.any? { |line| line.join.include?(player * win) }
   end
 end
