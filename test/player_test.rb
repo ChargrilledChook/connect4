57c1a73765abfc4b2c1198c2434c652d1f537260
chook <dylanw212@gmail.com>
2020-08-31 15:34:45 +1000

    add pseudocode for player tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -2,4 +2,13 @@ require 'minitest/autorun'
 require_relative '../lib/player'
 
 class PlayerTest < Minitest::Test
+
+  # Sends correct message with valid input
+
+  # Reprompts + sends with wrong input - numbers
+
+  # Reprompts + sends with wrong input - strings
+
+  # Reprompts and sends if receives false (ie column full)
+
 end
