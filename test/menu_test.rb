38f8f68c1d953932a06cde41e4e09865d4c895bc
chook <dylanw212@gmail.com>
2020-08-31 16:14:11 +1000

    add pseudocode for menu tests

--- test/menu_test.rb
+++ test/menu_test.rb
@@ -2,4 +2,10 @@ require 'minitest/autorun'
 require_relative './lib/menu'
 
 class MenuTest < Minitest::Test
+
+  # Starts a new game correctly
+
+  # Restarts a game correctly
+
+  # Exits game correctly
 end
